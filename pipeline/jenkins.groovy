867ff2573b26e95f40ff70b81d81f888f8b15c86
Andrii Korsun <korsuna.dev@gmail.com>
2025-08-28 20:38:48 +0000

    fix

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -9,7 +9,7 @@ pipeline {
     }
 
     environment {
-        GITHUB_TOKEN=credentials('github)
+        GITHUB_TOKEN=credentials('github')
         REPO = 'https://github.com/kors-dev/kbot.git'
         BRANCH = 'develop'
     }
